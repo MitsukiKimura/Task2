7ea8edb2966fabe40068840fee73339e577a636d
木村 光希 <180012@jc-21.jp>
2021-05-24 11:18:21 +0900

    はっきょã-

---
+++ src/test/java/jp/te4a/spring/boot/myapp2/AppTest.java
@@ -0,0 +1,23 @@
+package jp.te4a.spring.boot.myapp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void helloSpringWorldTest()
+    {
+        HelloController hc = new HelloController();
+        String expected = "Taro desu!";
+        String actual = hc.taro();
+        assertEquals(actual, expected);
+    }
+}
